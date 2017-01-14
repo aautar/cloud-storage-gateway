40e7ac3c9b9f525e4262640fcc9f7febc34fab63
Avishkar Autar <aautar@digital-radiation.com>
2017-01-14 12:50:43 -0500

    tests for ObjectInfo

--- tests/ObjectInfoTest.php
+++ tests/ObjectInfoTest.php
@@ -12,4 +12,22 @@ class ObjectInfoTest extends \PHPUnit_Framework_TestCase
         $objInfo = new ObjectInfo("content-type", 111, "tag", new DateTime("2016-01-01"));
         $this->assertEquals("content-type", $objInfo->getContentType());
     }
+    
+    public function testGetContentLengthReturnsContentLength()
+    {
+        $objInfo = new ObjectInfo("content-type", 111, "tag", new DateTime("2016-01-01"));
+        $this->assertEquals(111, $objInfo->getContentLength());
+    }    
+    
+    public function testGetETagReturnsETag()
+    {
+        $objInfo = new ObjectInfo("content-type", 111, "tag", new DateTime("2016-01-01"));
+        $this->assertEquals("tag", $objInfo->getETag());
+    }
+    
+    public function testGetLetLastModifiedReturnsLastModifiedTimestamp()
+    {
+        $objInfo = new ObjectInfo("content-type", 111, "tag", new DateTime("2016-01-01"));
+        $this->assertEquals(new DateTime("2016-01-01"), $objInfo->getLastModified());
+    }        
 }
