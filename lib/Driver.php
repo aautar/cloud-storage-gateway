69609d572a283c6524a5a5080b321eb45a00c778
Avishkar Autar <aautar@fragmentspace.com>
2024-07-07 17:08:25 -0400

    add type declarations to Driver interface

--- lib/Driver.php
+++ lib/Driver.php
@@ -9,21 +9,21 @@ interface Driver
      * @param string $bucket
      * @return boolean
      */
-    public function isBucketAccessible($bucket);
+    public function isBucketAccessible(string $bucket): bool;
 
     /**
      * @param string $bucket
      * @param string $key
      * @return ObjectInfo|null
      */
-    public function getObjectInfo($bucket, $key);
+    public function getObjectInfo(string $bucket, string $key): ?ObjectInfo;
 
     /**
      * @param string $bucket
      * @param string $key
-     * @return string
+     * @return string|null
      */
-    public function getObjectAsString($bucket, $key);
+    public function getObjectAsString(string $bucket, string $key): ?string;
 
     /**
      * @param string $bucket
@@ -31,23 +31,23 @@ interface Driver
      * @param string $saveToFile
      * @return bool
      */
-    public function getObjectAsFile($bucket, $key, $saveToFile);
+    public function getObjectAsFile(string $bucket, string $key, string $saveToFile): bool;
 
     /**
      * @param string $bucket
      * @param string $key
-     * @param int lifetime in seconds
+     * @param int $lifetimeSec lifetime in seconds
      * @return string
      */
-    public function getObjectAuthenticatedURL($bucket, $key, $lifetimeSec=300);
+    public function getObjectAuthenticatedURL(string $bucket, string $key, int $lifetimeSec=300): string;
 
     /**
      * @param string $bucket
      * @param string $key
-     * @param int lifetime in seconds
+     * @param int $lifetimeSec lifetime in seconds
      * @return string
      */
-    public function getPutObjectPresignedURL($bucket, $key, $lifetimeSec=300);
+    public function getPutObjectPresignedURL(string $bucket, string $key, int $lifetimeSec=300): string;
 
     /**
      *
@@ -62,7 +62,7 @@ interface Driver
      * @param string $key
      * @return bool
      */
-    public function deleteObject($bucket, $key);
+    public function deleteObject(string $bucket, string $key): bool;
 
     /**
      * @param string $fromBucket
@@ -71,7 +71,7 @@ interface Driver
      * @param string $toKey
      * @return bool
      */
-    public function copyObject($fromBucket, $fromKey, $toBucket, $toKey);
+    public function copyObject(string $fromBucket, string $fromKey, string $toBucket, string $toKey): bool;
 
     /**
      *
@@ -82,7 +82,7 @@ interface Driver
      * @param bool $isPublic
      * @return bool
      */
-    public function putObjectFromString($data, $bucket, $key, $contentType=null, $isPublic=false);
+    public function putObjectFromString(string $data, string $bucket, string $key, string $contentType=null, bool $isPublic=false): bool;
 
     /**
      *
@@ -93,5 +93,5 @@ interface Driver
      * @param bool $isPublic
      * @return bool
      */
-    public function putObjectFromFile($filepath, $bucket, $key, $contentType=null, $isPublic=false);
+    public function putObjectFromFile(string $filepath, string $bucket, string $key, string $contentType=null, bool $isPublic=false): bool;
 }
